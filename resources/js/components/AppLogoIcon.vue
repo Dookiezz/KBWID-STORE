37c166ee61bf571789dcbd2bcf3e8b2d9e7b8ee5
Admin <rizki.achanation@gmail.com>
2025-07-09 17:16:09 +0700

    initial commit

---
+++ resources/js/components/AppLogoIcon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+
+defineOptions({
+    inheritAttrs: false,
+});
+
+interface Props {
+    className?: HTMLAttributes['class'];
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+</template>
